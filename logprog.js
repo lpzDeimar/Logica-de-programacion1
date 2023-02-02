63046b427bc9babcf6f28763d4161fed6be2679d
Deimar Lopez <lpzdeimar@gmail.com>
2023-02-02 16:46:40 -0500

    [add] imagen

--- logprog.js
+++ logprog.js
@@ -2,20 +2,23 @@ let contador;
 const NUMBER = [];
 
 function getNumber() {
-    for (contador = 0; contador < 3; contador++) {
-        NUMBER.push(prompt("escriba un numero"));
-    }
-    console.log(NUMBER);
+  for (contador = 0; contador < 3; contador++) {
+    NUMBER.push(prompt("escriba un numero"));
+  }
+  console.log(NUMBER);
 }
 
 function compareNumber() {
-    if (NUMBER[0] === NUMBER[1] && NUMBER[2] && NUMBER[1] === NUMBER[2]) {
-        document.write(`sus numeros ${NUMBER} : Son iguales`)
-
-    } else {
-        document.write(`Orden asendente de sus numeros: ${(NUMBERO.sort((a, b) => a - b))}`);
-        document.write(`Orden asendente de sus numeros: ${(NUMBERO.sort((a, b) => a - b))}`);
-    }
+  if (NUMBER[0] === NUMBER[1] && NUMBER[2] && NUMBER[1] === NUMBER[2]) {
+    document.write(`sus numeros ${NUMBER} : Son iguales`);
+  } else {
+    document.write(
+      `Orden asendente de sus numeros: ${NUMBERO.sort((a, b) => a - b)}`
+    );
+    document.write(
+      `Orden asendente de sus numeros: ${NUMBERO.sort((a, b) => a - b)}`
+    );
+  }
 }
 
 getNumber();
